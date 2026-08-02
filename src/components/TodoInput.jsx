cdfb22bf3ede7a46d1a0cf3c67a6565b206fd534
omodudu <omodudu.ma@gmail.com>
2026-08-02 19:32:33 +0100

    fix todo-app button color on white background

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,18 +3,18 @@ import { useState } from 'react';
 export function TodoInput({ handleAddTodo, activeTab }) {
   const [inputValue, setInputValue] = useState('');
   return (
-    <div className="input-container">
+    <div className='input-container'>
       <input
         style={{ display: activeTab === 'Completed' ? 'none' : 'block' }}
-        type="text"
-        placeholder="Add Task"
+        type='text'
+        placeholder='Add Task'
         value={inputValue}
         onChange={e => {
           setInputValue(e.target.value);
         }}
       />
       <button
-        type="button"
+        type='button'
         style={{
           display: activeTab === 'Completed' ? 'none' : 'flex',
           paddingBlock: '7px',
@@ -34,11 +34,11 @@ export function TodoInput({ handleAddTodo, activeTab }) {
           handleAddTodo(inputValue);
           setInputValue('');
         }}>
-        <svg viewBox="0 0 448 512" width="30px" height="30px">
+        <svg viewBox='0 0 448 512' width='30px' height='30px'>
           <title>Add item</title>
           <path
-            fill="#ffffff"
-            d="M256 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-160 0 0-160z"
+            fill='currentColor'
+            d='M256 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 160-160 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0 0 160c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160 160 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-160 0 0-160z'
           />
         </svg>
       </button>
